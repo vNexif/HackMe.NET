96c808a6d12fcde98b3f6cf446ed1491a4103acf
Nexif <nexif@protonmail.com>
2021-06-20 22:34:55 +0200

    File Init

---
+++ HackMe.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackMe.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
