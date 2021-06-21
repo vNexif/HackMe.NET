3639cced0770d175ad4a83e7028f9b52b9f978a2
Nexif <nexif@protonmail.com>
2021-06-21 23:13:52 +0200

    Fix Ignores

---
+++ HackMe.NET/MultiThreading.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HackMe.NET
+{
+    class MultiThreading
+    {
+        public static void CallToChildThread()
+        {
+            try
+            {
+                Console.WriteLine("Child thread starts");
+
+                // do some work, like counting to 10
+                for (int counter = 0; counter <= 10; counter++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(counter);
+                }
+
+                Console.WriteLine("Child Thread Completed");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Thread Abort Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Couldn't catch the Thread Exception");
+            }
+        }
+
+        public static void method1()
+        {
+
+            // It prints numbers from 0 to 10
+            for (int I = 0; I <= 10; I++)
+            {
+                Console.WriteLine("Method1 is : {0}", I);
+
+                // When the value of I is equal to 5 then
+                // this method sleeps for 6 seconds
+                Thread.Sleep(600);
+ 
+            }
+        }
+
+        // static method two
+
+        public static void method2()
+        {
+            // It prints numbers from 0 to 10
+            for (int J = 0; J <= 10; J++)
+            {
+                Console.WriteLine("Method2 is : {0}", J);
+                Thread.Sleep(200);
+            }
+        }
+
+        public Thread thr1 = new Thread(method1);
+        public Thread thr2 = new Thread(method2);
+    }
+}
--- HackMe.NET/Program.cs
+++ HackMe.NET/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
 
 namespace HackMe.NET
 {
     class Program
     {
-        static void Main(string[] args)
+        class ThreadCreationProgram
         {
-            Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+                
+             
+                MultiThreading thrm = new MultiThreading();
+             
+                thrm.thr1.Start();
+                thrm.thr2.Start();
+
+
+                Process P = new Process();
+                
+                ProcessStartInfo Task1_StartInfo = new ProcessStartInfo();
+                Task1_StartInfo.FileName = "./HackMe.Net_Task1";
+                Task1_StartInfo.Arguments = "Hack";
+                P.StartInfo = Task1_StartInfo;
+                P.Start();
+                //P.Threads.AsParallel();
+                P.WaitForExit();
+                var result = P.ExitCode;
+                Console.WriteLine(result);
+                
+
+               /* Process pipeClient = new Process();
+
+                pipeClient.StartInfo.FileName = "./HackMe.Net_Task1.exe";
+
+                using (AnonymousPipeServerStream pipeServer =
+                    new AnonymousPipeServerStream(PipeDirection.Out,
+                        HandleInheritability.Inheritable))
+                {
+                    Console.WriteLine("[SERVER] Current TransmissionMode: {0}.",
+                        pipeServer.TransmissionMode);
+
+                    // Pass the client process a handle to the server.
+                    pipeClient.StartInfo.Arguments =
+                        pipeServer.GetClientHandleAsString();
+                    pipeClient.StartInfo.UseShellExecute = false;
+                    pipeClient.WaitForExitAsync();
+                    pipeClient.Start();
+
+                    pipeServer.DisposeLocalCopyOfClientHandle();
+
+                    try
+                    {
+                        // Read user input and send that to the client process.
+                        using (StreamWriter sw = new StreamWriter(pipeServer))
+                        {
+                            sw.AutoFlush = true;
+                            // Send a 'sync message' and wait for client to receive it.
+                            sw.WriteLine("SYNC");
+                            pipeServer.WaitForPipeDrain();
+                            // Send the console input to the client process.
+                            Console.Write("[SERVER] Enter text: ");
+                            sw.WriteLine(Console.ReadLine());
+                        }
+                    }
+                    // Catch the IOException that is raised if the pipe is broken
+                    // or disconnected.
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("[SERVER] Error: {0}", e.Message);
+                    }
+                }
+
+                pipeClient.WaitForExit();
+                pipeClient.Close();
+                Console.WriteLine("[SERVER] Client quit. Server terminating.");
+
+
+                Console.ReadKey();
+               */
+            }
         }
     }
 }
