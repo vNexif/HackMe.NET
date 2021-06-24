cc774ed37b33871faae81f7b28f4fdf71e9ee248
El-Patron-Salan <salan2000@wp.pl>
2021-06-24 02:27:29 +0200

    SQL_connect
    connection

---
+++ HackMe.Net_Task2DB/DB_tasks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HackMe.Net_Task2DB
+{
+    class DB_task
+    {
+
+        public void connect_to_db(string database)
+        {
+
+            string connectionPath;
+            SqlConnection connect;
+
+            var data_source = $@"{database}";
+            var database_name = "HackMe";
+            /*(
+            var user_name = "HackerBoi";
+            var password = "psswd";
+
+            connectionPath = @"Data Source=" + data_source + ";Initial Catalog=" + database_name +
+                             ";Persist Security Info=True;User ID=" + user_name + ";Password" + password;
+            */
+            connectionPath = @"Data Source=" + data_source + ";Initial Catalog=" + database_name +
+                             ";Integrated Security=True;MultipleActiveResultSets=True";
+            connect = new(connectionPath);
+            //connect.Open();
+        }
+    }
+}
